245eb9250f625d323772f49abfaa9853885b9c72
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2023-08-13 15:57:38 +0300

    T2-213 Create endpoints for post model (create, list, retrieve, destroy)

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import mixins, status
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from .models import Post
+from .serializers import PostSerializer, PostCreateSerializer
+
+
+class PostModelViewSet(mixins.RetrieveModelMixin,
+                       mixins.DestroyModelMixin,
+                       mixins.ListModelMixin,
+                       GenericViewSet):
+    """API view for post model instances (List/Retrieve/Destroy)."""
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    @swagger_auto_schema(
+        request_body=PostCreateSerializer,
+        operation_description="This endpoint creates new post.",
+        responses={
+            201: 'Post added successfully.',
+            400: 'Bad Request'
+        }
+    )
+    def create(self, request, *args, **kwargs):
+        user = self.request.user
+        request.data["user_id"] = user.id
+        serializer = PostCreateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'message': 'Post added successfully.'}, status=status.HTTP_201_CREATED)
