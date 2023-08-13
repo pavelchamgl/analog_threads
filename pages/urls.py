aad644115b523b8655dd382c6e74d5cce29535f5
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2023-08-13 22:20:03 +0300

    T2-39 Added url for like/unlike endpoint

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,7 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
 
-from .views import PostModelViewSet
+from .views import PostModelViewSet, PostLikeUnlikeAPIView
 
 
 router = routers.DefaultRouter()
@@ -10,5 +10,5 @@ router.register('', PostModelViewSet, basename='post')
 
 urlpatterns = [
     path('post/', include(router.urls)),
-
+    re_path('post/like_unlike/(?P<post_id>.+)/', PostLikeUnlikeAPIView.as_view(), name='post_like_unlike'),
 ]
