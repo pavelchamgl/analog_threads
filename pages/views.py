69c9779eae67cb520737ec3fdcdf9ca1fd2d4810
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2023-08-13 22:18:19 +0300

    T2-39 Added like/unlike endpoint

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from rest_framework import mixins, status
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
 
 from .models import Post
@@ -17,7 +18,7 @@ class PostModelViewSet(mixins.CreateModelMixin,
     API view for user post model instances (List/Retrieve/Destroy).
     """
     serializer_class = PostSerializer
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         user_id = self.request.user.id
@@ -39,3 +40,28 @@ class PostModelViewSet(mixins.CreateModelMixin,
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response({'message': 'Post added successfully.'}, status=status.HTTP_201_CREATED)
+
+
+class PostLikeUnlikeAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(
+        operation_description="This endpoint like/unlike post.",
+        responses={
+            200: 'Like added. / Like removed.',
+            404: 'Post not found.'
+        }
+    )
+    def patch(self, request, post_id):
+        user = request.user
+
+        try:
+            post = Post.objects.get(id=post_id)
+        except Post.DoesNotExist:
+            return Response({'error': 'Post not found.'}, status=status.HTTP_404_NOT_FOUND)
+        if not post.likes.filter(id=user.id).exists():
+            post.likes.add(user.id)
+            return Response({'message': 'Like added.'}, status=status.HTTP_200_OK)
+        elif post.likes.filter(id=user.id).exists():
+            post.likes.remove(user.id)
+            return Response({'success': 'Like removed.'}, status=status.HTTP_200_OK)
