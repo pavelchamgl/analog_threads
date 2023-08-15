be030c0f7f9b244edc42e801cc554edb66275482
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2023-08-15 11:51:44 +0300

    T2-41 Created serializers for comment model

--- pages/serializers.py
+++ pages/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 
-from .models import Post
+from .models import Post, Comment
 
 
 class RepostSerializer(serializers.ModelSerializer):
@@ -32,3 +32,30 @@ class PostCreateSerializer(serializers.ModelSerializer):
         model = Post
         fields = ['author', 'text', 'date_posted', 'repost', 'comments_permission']
 
+
+class ReplySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'author', 'text', 'date_posted' ]
+
+
+class CommentViewSerializer(serializers.ModelSerializer):
+    reply = ReplySerializer()
+    total_likes = SerializerMethodField()
+    user_like = SerializerMethodField()
+
+    def get_total_likes(self, obj):
+        return obj.total_likes()
+
+    def get_user_like(self, obj):
+        return obj.user_like(self.context['request'].user)
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'author', 'text', 'date_posted', 'reply', 'total_likes', 'user_like']
+
+
+class CommentCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'author', 'text', 'date_posted', 'reply', ]
