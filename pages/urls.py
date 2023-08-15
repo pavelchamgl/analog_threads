4042e83b62229350b9b3b03dcc02d620db1dc5b6
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2023-08-15 11:57:32 +0300

    T2-41 Added url for comment

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include, re_path
 from rest_framework import routers
 
-from .views import PostModelViewSet, PostLikeUnlikeAPIView
+from .views import PostModelViewSet, PostLikeUnlikeAPIView, CommentListCreateAPIView
 
 
 router = routers.DefaultRouter()
@@ -11,4 +11,5 @@ router.register('', PostModelViewSet, basename='post')
 urlpatterns = [
     path('post/', include(router.urls)),
     re_path('post/like_unlike/(?P<post_id>.+)/', PostLikeUnlikeAPIView.as_view(), name='post_like_unlike'),
+    re_path('post/(?P<post_id>.+)/comments/', CommentListCreateAPIView.as_view(), name='comment-list-create'),
 ]
