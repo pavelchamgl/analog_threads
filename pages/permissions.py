8003695f76af0d3e9049cbc22d5910350d8a6896
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2023-08-18 09:42:18 +0300

    T2-41 Created permission for create reply

--- pages/permissions.py
+++ pages/permissions.py
@@ -1,12 +1,28 @@
 from rest_framework.permissions import BasePermission
 
-from .models import Post
+from .models import Post, Comment
 
 
 class CommentPermission(BasePermission):
     def has_permission(self, request, view):
         post_id = view.kwargs.get('post_id')
         post = Post.objects.get(id=post_id)
+        if post.comments_permission == 'anyone':
+            return True
+        elif post.comments_permission == 'your followers':
+            return post.author.follower.filter(followee=request.user).exists()
+        elif post.comments_permission == 'profiles you follow':
+            return post.author.followee.filter(follower=request.user).exists()
+        elif post.comments_permission == 'mentioned only':
+            return post.mentioned_users.filter(id=request.user.id).exists()
+        return False
+
+
+class ReplyPermission(BasePermission):
+    def has_permission(self, request, view):
+        comment_id = view.kwargs.get('comment_id')
+        comment = Comment.objects.get(id=comment_id)
+        post = Post.objects.get(id=comment.post.id)
         if post.comments_permission == 'anyone':
             return True
         elif post.comments_permission == 'your followers':
