f73470ab0cedc5b67153285cf1a8ae09eb603520
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2023-08-19 20:06:41 +0300

    T2-41 Update permissions for comment and reply

--- pages/permissions.py
+++ pages/permissions.py
@@ -7,15 +7,7 @@ class CommentPermission(BasePermission):
     def has_permission(self, request, view):
         post_id = view.kwargs.get('post_id')
         post = Post.objects.get(id=post_id)
-        if post.comments_permission == 'anyone':
-            return True
-        elif post.comments_permission == 'your followers':
-            return post.author.follower.filter(followee=request.user).exists()
-        elif post.comments_permission == 'profiles you follow':
-            return post.author.followee.filter(follower=request.user).exists()
-        elif post.comments_permission == 'mentioned only':
-            return post.mentioned_users.filter(id=request.user.id).exists()
-        return False
+        return permission_comment(post, request.user)
 
 
 class ReplyPermission(BasePermission):
@@ -23,12 +15,16 @@ class ReplyPermission(BasePermission):
         comment_id = view.kwargs.get('comment_id')
         comment = Comment.objects.get(id=comment_id)
         post = Post.objects.get(id=comment.post.id)
-        if post.comments_permission == 'anyone':
-            return True
-        elif post.comments_permission == 'your followers':
-            return post.author.follower.filter(followee=request.user).exists()
-        elif post.comments_permission == 'profiles you follow':
-            return post.author.followee.filter(follower=request.user).exists()
-        elif post.comments_permission == 'mentioned only':
-            return post.mentioned_users.filter(id=request.user.id).exists()
-        return False
+        return permission_comment(post, request.user)
+
+
+def permission_comment(post, user):
+    if post.comments_permission == 'anyone':
+        return True
+    elif post.comments_permission == 'your followers':
+        return post.author.follower.filter(followee=user).exists()
+    elif post.comments_permission == 'profiles you follow':
+        return post.author.followee.filter(follower=user).exists()
+    elif post.comments_permission == 'mentioned only':
+        return post.mentioned_users.filter(id=user.id).exists()
+    return False
