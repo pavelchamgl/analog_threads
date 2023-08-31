78a540c3a55ee4939eee0adebe4ed20f75f647d9
Ihor <enotshow275@gmail.com>
2023-08-31 19:42:58 +0200

    Update url service
    - Update url service to new one

--- pages/urls.py
+++ pages/urls.py
@@ -9,11 +9,11 @@ router.register('', views.PostModelViewSet, basename='post')
 
 urlpatterns = [
     path('post/', include(router.urls)),
-    re_path('post/like_unlike/(?P<post_id>.+)/', views.PostLikeUnlikeAPIView.as_view(), name='post_like_unlike'),
-    re_path('post/(?P<post_id>.+)/comments/', views.CommentListCreateAPIView.as_view(), name='comment-list-create'),
-    re_path('post/comments/(?P<comment_id>.+)/reply/', views.ReplyCreateAPIView.as_view(), name='reply'),
-    re_path('post/(?P<post_id>.+)/repost/', views.RepostCreateAPIVIew.as_view(), name='repost_create'),
-    re_path('post/(?P<post_id>.+)/quote/', views.QuoteCreateAPIVIew.as_view(), name='quote_create'),
+    path('post/like_unlike/<post_id>/', views.PostLikeUnlikeAPIView.as_view(), name='post_like_unlike'),
+    path('post/<post_id>/comments/', views.CommentListCreateAPIView.as_view(), name='comment-list-create'),
+    path('post/comments/<comment_id>/reply/', views.ReplyCreateAPIView.as_view(), name='reply'),
+    path('post/<post_id>/repost/', views.RepostCreateAPIVIew.as_view(), name='repost_create'),
+    path('post/<post_id>/quote/', views.QuoteCreateAPIVIew.as_view(), name='quote_create'),
 
     path('feed/for_you/', views.ForYouFeedView.as_view(), name='for_you_feed'),
     path('feed/following/', views.FollowingFeedView.as_view(), name='following_feed'),
