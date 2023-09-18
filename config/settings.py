9417151b13ea48e9e1b74346a209ad8cc849423c
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2023-09-18 20:52:12 +0300

    Added static_root param in settings

--- config/settings.py
+++ config/settings.py
@@ -208,6 +208,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 CLOUDINARY_URL = os.environ['CLOUDINARY_URL']
 
