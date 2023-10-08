86d3ea9bfa8fd38b6b4b10eef9f100ff26c9844c
Pavel Ivanov <pavel.ivanov.060988@gmail.com>
2023-10-08 19:03:26 +0300

    Added user posts endpoint URL

--- pages/urls.py
+++ pages/urls.py
@@ -9,6 +9,7 @@ router.register('', views.PostModelViewSet, basename='post')
 
 urlpatterns = [
     path('post/', include(router.urls)),
+    re_path('post/(?P<user_id>.+)/list/', views.UserPostsListAPIView.as_view(), name='posts-by-user_id'),
     re_path('post/(?P<post_id>.+)/view/', views.PostDetailAPIView.as_view(), name='post-detail'),
     re_path('post/like_unlike/(?P<post_id>.+)/', views.PostLikeUnlikeAPIView.as_view(), name='post_like_unlike'),
     re_path('post/(?P<post_id>.+)/comments/', views.CommentListCreateAPIView.as_view(), name='comment-list-create'),
